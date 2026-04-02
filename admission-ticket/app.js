3ebc52e3ede86652944e6c90694eb9a5477b5610
Ngamlubou Ringkangmai <ngamluboungamlubou@gmail.com>
2026-04-02 22:53:36 +0530

    Update ticket code generation and display logic

--- admission-ticket/app.js
+++ admission-ticket/app.js
@@ -2,31 +2,25 @@ const form = document.getElementById('admissionForm');
 const ticketInfo = document.getElementById('ticketInfo');
 
 function generate3Digit() {
-  return Math.floor(Math.random() * 1000)
+  return Math.floor(Math.random() * 10000)
     .toString()
-    .padStart(3, "0");
+    .padStart(4, "0");
 }
 
 form.addEventListener('submit', function(e) {
   e.preventDefault();
 
-  const status = document.querySelector('input[name="status"]:checked').value;
-
 const sClass = document.getElementById('classSelect');
 
 const classText = sClass.selectedOptions[0].text;
 
-const statusLetter = status === "newcomer" ? "N" : "C";
-
-const code = statusLetter + sClass.value + generate3Digit();
+const code = sClass.value + generate3Digit();
 
   const ticketHTML = `
     <h3>Ticket Details</h3>
-    <p><strong>Status:</strong> ${statusLetter}</p>
 <p><strong>Class:</strong> ${classText}</p>
-    <p><strong>Code:</strong></p>
+    <p><strong>Code:</strong><span>Use this code for admission.</span></p>
   <h2>${code}</h2>
-    <p>Use this code for admission.</p>
   `;
 
   ticketInfo.innerHTML = ticketHTML;
