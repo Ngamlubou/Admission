bcbb9c68eef4cdf266e0e4b5f7e8092103311993
Ngamlubou Ringkangmai <ngamluboungamlubou@gmail.com>
2026-04-06 23:30:29 +0530

    Refactor admission form submission logic
    Refactor admission form submission to use async/await and update ticket code retrieval.

--- admission-ticket/app.js
+++ admission-ticket/app.js
@@ -1,56 +1,32 @@
-const form = document.getElementById('admissionForm');
-const ticketInfo = document.getElementById('ticketInfo');
-
-document.getElementById('payBtn').addEventListener('click', function () {
-  const options = {
- key: "rzp_test_SYuOBsX5gApP7v", 
-    amount: 10000, 
-    currency: "INR",
-    name: "Demo Payment",
-    description: "Test Transaction",
-    handler: function (response) {
-      alert("Payment successful! Payment ID: " + response.razorpay_payment_id);
-       },
-    prefill: {
-      name: "John Doe",
-      email: "john@example.com",
-      contact: "9999999999"
-    },
-method: {
-    upi: true,
-    card: false,
-    netbanking: false,
-    wallet: false
-  },
-    theme: {
-      color: "#3399cc"  }
-  };
-const rzp = new Razorpay(options);
-  rzp.open();
-});
-
-function generate3Digit() {
-  return Math.floor(Math.random() * 10000)
-    .toString()
-    .padStart(4, "0");
-}
-
-form.addEventListener('submit', function(e) {
+form.addEventListener('submit', async function(e) {
   e.preventDefault();
 
-const sClass = document.getElementById('classSelect');
+  const sClass = document.getElementById('classSelect');
+  const classText = sClass.selectedOptions[0].text;
+  const classValue = sClass.value;
 
-const classText = sClass.selectedOptions[0].text;
+  try {
+    const res = await fetch("https://smartpea-backend.onrender.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        classValue: classValue
+      })
+    });
 
-const code = sClass.value + generate3Digit();
+    const data = await res.json();
 
-  const ticketHTML = `
-    <h3>Ticket Details</h3>
-<p><strong>Class:</strong> ${classText}</p>
-    <p><strong>Code:</strong><span>Use this code for admission.</span></p>
-  <h2>${code}</h2>
-  `;
+    console.log("Backend response:", data);
 
-  ticketInfo.innerHTML = ticketHTML;
-  ticketInfo.style.display = 'block';
+    const ticketHTML = `
+      <h3>Ticket Details</h3>
+      <p><strong>Class:</strong> ${classText}</p>
+      <p><strong>Code:</strong><span>Use this code for admission.</span></p>
+      <h2>${data.code}</h2>
+    `;
+    ticketInfo.innerHTML = ticketHTML;
+    ticketInfo.style.display = 'block';
+  } 
 });
